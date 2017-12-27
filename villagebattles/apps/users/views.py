65033e8206ffec646e5a8183c0f93fe3dd1ddab7
Eric Wang <ezwang1@gmail.com>
2017-12-27 15:18:14 -0500

    implement registration and login

--- villagebattles/apps/users/views.py
+++ villagebattles/apps/users/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login as auth_login, authenticate
+from .forms import UserCreationForm
 
 
 def index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated:
+        return render(request, "dashboard.html")
+    else:
+        return render(request, "index.html")
 
 
 def login(request):
     if request.method == "POST":
-        messages.error(request, "Login failed! Is your username and password correct?")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            auth_login(request, user)
+        else:
+            messages.error(request, "Login failed! Is your username and password correct?")
     return redirect("index")
 
 
 def register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            messages.success(request, "Your user account has been created! Check your email to continue.")
+            return redirect("index")
+        else:
+            messages.error(request, "There were errors while trying to create your account.")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
