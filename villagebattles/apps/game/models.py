b631557d475fcd5bf142d1ae5d9a21bab5687357
Eric Wang <ezwang1@gmail.com>
2017-12-27 15:33:19 -0500

    add village model

--- villagebattles/apps/game/models.py
+++ villagebattles/apps/game/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+from ..users.models import User
+
 
 class World(models.Model):
     name = models.TextField()
 
     def __str__(self):
         return self.name
+
+
+class Village(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    name = models.TextField()
+    world = models.ForeignKey(World, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        unique_together = (("x", "y"),)
