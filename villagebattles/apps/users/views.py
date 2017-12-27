2eaf35bccb0ca0310afe15379bd470abc8f8edf4
Eric Wang <ezwang1@gmail.com>
2017-12-27 14:54:07 -0500

    style login page

--- villagebattles/apps/users/views.py
+++ villagebattles/apps/users/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def login(request):
+    if request.method == "POST":
+        pass
+    return redirect("index")
