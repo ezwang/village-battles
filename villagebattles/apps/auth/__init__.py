ad64e2d94c154e47f44239b5095cf1da998e19a3
Eric Wang <ezwang1@gmail.com>
2017-12-27 14:01:56 -0500

    add auth app

---
+++ villagebattles/apps/auth/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "villagebattles.apps.auth.apps.AuthConfig"
---
+++ villagebattles/apps/auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthConfig(AppConfig):
+    name = 'villagebattles.apps.auth'
+    label = 'villagebattles.apps.auth'
