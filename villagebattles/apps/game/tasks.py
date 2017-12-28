ad926a51e3da03420d45c714bd7a1f44f3c99467
Eric Wang <ezwang1@gmail.com>
2017-12-28 09:34:01 -0500

    set timezone

--- villagebattles/apps/game/tasks.py
+++ villagebattles/apps/game/tasks.py
@@ -8,7 +8,7 @@ from .constants import get_building_upgrade_time
 def process(villages):
     now = timezone.now()
     for village in villages:
-        finished = village.buildqueue.filter(end_time__gte=now).order_by("end_time")
+        finished = village.buildqueue.filter(end_time__lte=now).order_by("end_time")
         for done in finished:
             build = village.buildings.filter(type=done.type)
             if build.exists():
@@ -24,6 +24,7 @@ def process(villages):
             done.delete()
         existing = village.buildqueue.filter(end_time__isnull=False).count()
         for task in village.buildqueue.order_by("start_time")[:2-existing]:
-            task.end_time = now + timedelta(seconds=get_building_upgrade_time(task.type, task.new_level - 1))
+            build_time = timedelta(seconds=get_building_upgrade_time(task.type, task.new_level - 1))
+            task.end_time = now + build_time
             task.save()
         village._do_resource_update()
