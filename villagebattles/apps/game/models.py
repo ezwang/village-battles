de81a572a48c6fb20e15b8b7918546b48eb512e3
Eric Wang <ezwang1@gmail.com>
2017-12-27 21:55:23 -0500

    implement resources

--- villagebattles/apps/game/models.py
+++ villagebattles/apps/game/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
@@ -20,11 +22,67 @@ class Village(models.Model):
     world = models.ForeignKey(World, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    _wood = models.IntegerField(default=settings.STARTING_RESOURCES)
-    _clay = models.IntegerField(default=settings.STARTING_RESOURCES)
-    _iron = models.IntegerField(default=settings.STARTING_RESOURCES)
+    _wood = models.DecimalField(max_digits=16, decimal_places=6, default=settings.STARTING_RESOURCES)
+    _clay = models.DecimalField(max_digits=16, decimal_places=6, default=settings.STARTING_RESOURCES)
+    _iron = models.DecimalField(max_digits=16, decimal_places=6, default=settings.STARTING_RESOURCES)
 
     _update = models.DateTimeField(default=timezone.now)
+
+    @property
+    def max_capacity(self):
+        return 1000
+
+    def _do_resource_update(self):
+        if hasattr(self, "_done_resource_update"):
+            return
+        self._done_resource_update = True
+        now = timezone.now()
+        diff = (now - self._update)
+        diff = int(diff.total_seconds()) + (diff.microseconds / Decimal(1000000))
+        self.wood = self._wood + diff * self.wood_rate
+        self.clay = self._clay + diff * self.clay_rate
+        self.iron = self._iron + diff * self.iron_rate
+        self._update = now
+        self.save()
+
+    @property
+    def wood(self):
+        self._do_resource_update()
+        return int(self._wood)
+
+    @property
+    def clay(self):
+        self._do_resource_update()
+        return int(self._clay)
+
+    @property
+    def iron(self):
+        self._do_resource_update()
+        return int(self._iron)
+
+    @property
+    def wood_rate(self):
+        return 1
+
+    @property
+    def clay_rate(self):
+        return 1
+
+    @property
+    def iron_rate(self):
+        return 1
+
+    @wood.setter
+    def wood(self, x):
+        self._wood = min(x, self.max_capacity)
+
+    @clay.setter
+    def clay(self, x):
+        self._clay = min(x, self.max_capacity)
+
+    @iron.setter
+    def iron(self, x):
+        self._iron = min(x, self.max_capacity)
 
     class Meta:
         unique_together = (("x", "y"),)
