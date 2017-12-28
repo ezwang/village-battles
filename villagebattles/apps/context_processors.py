6ca16ca0ca4400db5e2cea84d8f6c6db0cd6c573
Eric Wang <ezwang1@gmail.com>
2017-12-28 09:37:26 -0500

    prefetch related buildings

--- villagebattles/apps/context_processors.py
+++ villagebattles/apps/context_processors.py
@@ -8,7 +8,7 @@ def world_info(request):
     if "world" in request.session:
         out["world"] = World.objects.get(id=request.session["world"])
     if "village" in request.session:
-        out["current_village"] = Village.objects.get(id=request.session["village"])
+        out["current_village"] = Village.objects.prefetch_related("buildings").get(id=request.session["village"])
     return out
 
 
