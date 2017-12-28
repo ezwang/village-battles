647976ab9c9485168966948139ad656a9ec396dc
Eric Wang <ezwang1@gmail.com>
2017-12-27 22:09:09 -0500

    add farm migration

--- villagebattles/apps/game/tests.py
+++ villagebattles/apps/game/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import World, Village
+from ..users.models import User
+
+
+class ResourceTests(TestCase):
+    def setUp(self):
+        self.world = World.objects.create(name="World 1")
+        self.user = User.objects.create_user(username="test", password="test")
+
+    def test_rate_correct(self):
+        village = Village.objects.create(
+            x=500,
+            y=500,
+            name="Test Village",
+            world=self.world,
+            user=self.user
+        )
