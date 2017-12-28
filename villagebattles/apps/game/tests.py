7bbc00056e99b3f0f3407ef63cfd4cfe47190b1e
Eric Wang <ezwang1@gmail.com>
2017-12-28 17:31:02 -0500

    add test cases for loot updates

--- villagebattles/apps/game/tests.py
+++ villagebattles/apps/game/tests.py
@@ -3,26 +3,67 @@ from datetime import timedelta
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import World, Village
+from .models import World, Village, Attack
 from ..users.models import User
+from .tasks import process_village
 
 
 class ResourceTests(TestCase):
     def setUp(self):
         self.world = World.objects.create(name="World 1")
         self.user = User.objects.create_user(username="test", password="test")
-
-    def test_auto_update(self):
-        """ Make sure resource values are being updated on each call. """
-        village = Village.objects.create(
+        self.user2 = User.objects.create_user(username="test2", password="test")
+        self.village = Village.objects.create(
             x=500,
             y=500,
             name="Test Village",
             world=self.world,
             owner=self.user
         )
-        initial_wood = village._wood
-        expected_wood = min(initial_wood + village.wood_rate, village.max_capacity)
-        village._update = timezone.now() - timedelta(hours=1)
-        village.save()
-        self.assertEquals(village.wood, expected_wood, (village.wood, expected_wood))
+        self.village2 = Village.objects.create(
+            x=501,
+            y=501,
+            name="Test Village 2",
+            world=self.world,
+            owner=self.user2
+        )
+
+    def test_auto_update(self):
+        """ Make sure resource values are being updated on each call. """
+        initial_wood = self.village._wood
+        expected_wood = min(initial_wood + self.village.wood_rate, self.village.max_capacity)
+        self.village._update = timezone.now() - timedelta(hours=1)
+        self.village.save()
+        self.assertEquals(self.village.wood, expected_wood, (self.village.wood, expected_wood))
+
+    def test_update_with_loot_first(self):
+        now = timezone.now()
+        initial_wood = self.village._wood
+        expected_wood = min(initial_wood + self.village.wood_rate + 100, self.village.max_capacity)
+        self.village._update = now - timedelta(hours=1)
+        self.village.save()
+        Attack.objects.create(
+            source=self.village,
+            destination=self.village2,
+            loot="100,100,100",
+            end_time=now - timedelta(minutes=30),
+            returning=True
+        )
+        process_village(self.village, now)
+        self.assertEquals(self.village.wood, expected_wood, (self.village.wood, expected_wood))
+
+    def test_update_with_loot_last(self):
+        now = timezone.now()
+        initial_wood = self.village._wood
+        expected_wood = min(initial_wood + self.village.wood_rate + 100, self.village.max_capacity)
+        self.village._update = now - timedelta(hours=1)
+        self.village.save()
+        Attack.objects.create(
+            source=self.village,
+            destination=self.village2,
+            loot="100,100,100",
+            end_time=now - timedelta(hours=2),
+            returning=True
+        )
+        process_village(self.village, now)
+        self.assertEquals(self.village.wood, expected_wood, (self.village.wood, expected_wood))
