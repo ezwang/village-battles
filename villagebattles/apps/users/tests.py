a9a9b2748501c85cee12f58f295da27c017a9779
Eric Wang <ezwang1@gmail.com>
2017-12-28 23:52:00 -0500

    test village creation

--- villagebattles/apps/users/tests.py
+++ villagebattles/apps/users/tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
-from django.test import Client
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from .models import User
 
 
-class BasicTests(TestCase):
+class UnauthenticatedTests(TestCase):
     def setUp(self):
         self.client = Client()
 
@@ -13,3 +15,21 @@ class BasicTests(TestCase):
     def test_register(self):
         response = self.client.get("/register")
         self.assertEqual(response.status_code, 200)
+
+
+class BasicTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(username="test", password="test")
+        self.client.login(username="test", password="test")
+        session = self.client.session
+        session["world"] = 1
+        session.save()
+
+    def test_world_creation(self):
+        response = self.client.get(reverse("start"))
+        self.assertRedirects(response, reverse("create_village"))
+
+    def test_village_creation(self):
+        response = self.client.post(reverse("create_village"))
+        self.assertRedirects(response, reverse("village", kwargs={"village_id": 1}))
