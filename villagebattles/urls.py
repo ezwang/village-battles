8ac430f2c9ac35750832ec04abd909af9b239283
Eric Wang <ezwang1@gmail.com>
2017-12-28 22:09:06 -0500

    set admin header

--- villagebattles/urls.py
+++ villagebattles/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Village Battles administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('villagebattles.apps.users.urls')),
