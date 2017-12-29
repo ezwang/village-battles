8dfa9b7ab9105436dc8359e3bc95d6f2a1e382d6
Eric Wang <ezwang1@gmail.com>
2017-12-29 00:01:51 -0500

    fix build

--- villagebattles/apps/game/helpers.py
+++ villagebattles/apps/game/helpers.py
@@ -20,18 +20,18 @@ def square_spiral(n):
     if n <= 1:
         return (0, 0)
     r = int((sqrt(n - 1) - 1)/2)
-    l = r*2 + 3
-    d = n - (l - 2)**2
-    if d <= l:
-        return (floor(-floor(l/2) + d) - 1, floor(l/2))
-    elif d <= l*2:
-        return (floor(-floor(l/2) + (d - l)) - 1, -floor(l/2))
-    elif d <= l*3-2:
-        dm = d - l*2
-        return (ceil(-l/2), ceil(-floor(l/2) + dm))
+    length = r*2 + 3
+    d = n - (length - 2)**2
+    if d <= length:
+        return (floor(-floor(length/2) + d) - 1, floor(length/2))
+    elif d <= length*2:
+        return (floor(-floor(length/2) + (d - length)) - 1, -floor(length/2))
+    elif d <= length*3-2:
+        dm = d - length*2
+        return (ceil(-length/2), ceil(-floor(length/2) + dm))
     else:
-        dm = d - (l*3-2)
-        return (floor(l/2), ceil(-floor(l/2) + dm))
+        dm = d - (length*3-2)
+        return (floor(length/2), ceil(-floor(length/2) + dm))
 
 
 def calculate_travel_time(source, destination, troops):
