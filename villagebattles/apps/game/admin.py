ef40b5e2eaeca050ad80665d80f92e9221d6e2f3
Eric Wang <ezwang1@gmail.com>
2017-12-28 22:07:25 -0500

    add models to admin

--- villagebattles/apps/game/admin.py
+++ villagebattles/apps/game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import World, Village, Building, Troop
+
+admin.site.register(World)
+admin.site.register(Village)
+admin.site.register(Building)
+admin.site.register(Troop)
