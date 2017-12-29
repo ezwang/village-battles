501ce2ad2100a041007b7b7050b7756d3026b5a4
Eric Wang <ezwang1@gmail.com>
2017-12-28 21:57:30 -0500

    add basic page tests

--- villagebattles/apps/users/tests.py
+++ villagebattles/apps/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class BasicTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        response = self.client.get("/register")
+        self.assertEqual(response.status_code, 200)
