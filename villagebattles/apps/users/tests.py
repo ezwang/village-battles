ddf7e79f9da5f43a5579f7edac39fc244b807b9f
Eric Wang <ezwang1@gmail.com>
2017-12-30 10:38:41 -0500

    add test for build

--- villagebattles/apps/users/tests.py
+++ villagebattles/apps/users/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.utils import timezone
+
+from mock import patch
+from datetime import timedelta
 
 from .models import User
-from ..game.models import Village, World
+from ..game.models import Village, World, Building
 
 
 class UnauthenticatedTests(TestCase):
@@ -38,18 +42,50 @@ class BasicTests(TestCase):
         response = self.client.get(reverse("settings"))
         self.assertEqual(response.status_code, 200)
 
-    def test_village_creation(self):
+    def do_enter_game(self):
         response = self.client.post(reverse("create_village"))
         self.assertEqual(Village.objects.all().count(), 1)
 
         village_id = Village.objects.get().id
         self.assertRedirects(response, reverse("village", kwargs={"village_id": village_id}))
 
-        response = self.client.get(reverse("hq", kwargs={"village_id": village_id}))
+        return Village.objects.get(id=village_id)
+
+    def test_village_creation(self):
+        """ Make sure that creating villages works. """
+        village = self.do_enter_game()
+
+        response = self.client.get(reverse("hq", kwargs={"village_id": village.id}))
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.get(reverse("rally", kwargs={"village_id": village_id}))
+        response = self.client.get(reverse("rally", kwargs={"village_id": village.id}))
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.get(reverse("barracks", kwargs={"village_id": village_id}))
-        self.assertRedirects(response, reverse("village", kwargs={"village_id": village_id}))
+        response = self.client.get(reverse("barracks", kwargs={"village_id": village.id}))
+        self.assertRedirects(response, reverse("village", kwargs={"village_id": village.id}))
+
+    def do_build(self, village, building):
+        old_count = village.buildqueue.count()
+        try:
+            data = {"building": village.buildings.get(type=building).id}
+        except Building.DoesNotExist:
+            data = {"build": building}
+        response = self.client.post(reverse("hq", kwargs={"village_id": village.id}), data)
+        self.assertRedirects(response, reverse("hq", kwargs={"village_id": village.id}))
+        self.assertEquals(village.buildqueue.count(), old_count + 1)
+
+    def test_building_upgrade(self):
+        """ Make sure that upgrading buildings works. """
+        village = self.do_enter_game()
+        now = timezone.now()
+
+        with patch.object(timezone, "now", return_value=now - timedelta(days=1)):
+            village._update = timezone.now()
+            village.save()
+
+            self.do_build(village, "HQ")
+
+        response = self.client.get(reverse("hq", kwargs={"village_id": village.id}))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEquals(village.buildings.get(type="HQ").level, 2)
