6b96737ceacaf63a961f71637959c8b91f4a8f7c
Eric Wang <ezwang1@gmail.com>
2017-12-30 15:02:58 -0500

    show attack and defense stats

--- villagebattles/apps/game/templatetags/game_extras.py
+++ villagebattles/apps/game/templatetags/game_extras.py
@@ -4,7 +4,8 @@ from django import template
 from django.utils.safestring import mark_safe
 
 from ..constants import (get_building_cost, get_building_population_difference, get_building_upgrade_time,
-                         get_troop_cost, get_troop_population, get_troop_time, get_hq_buff, get_recruitment_buff)
+                         get_troop_cost, get_troop_population, get_troop_time, get_hq_buff, get_recruitment_buff,
+                         get_troop_attack, get_troop_defense)
 
 register = template.Library()
 
@@ -41,6 +42,13 @@ def troop_time(troop, village):
     t = get_troop_time(troop)
     buff = get_recruitment_buff("BR", village.get_level("BR"))
     return str(timedelta(seconds=int(t * buff)))
+
+
+@register.simple_tag()
+def troop_stats(troop, village):
+    attack = get_troop_attack(troop)
+    defense = get_troop_defense(troop)
+    return mark_safe("<span class='attack'>{}</span><span class='defense'>{}</span>".format(attack, defense))
 
 
 @register.filter(name="can_build")
